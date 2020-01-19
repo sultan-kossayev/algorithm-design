77c5a655d60e0596a59789d4f31d8bd8115ef498
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-01-19 11:28:52 +0300

    remove the implementation of data structures

--- src/main/java/structures/IterableStructure.java
+++
@@ -1,9 +0,0 @@
-package structures;
-
-/**
- * The marker interface for data structures that are iterable
- *
- * @param <T> type of data
- */
-public interface IterableStructure<T> extends DataStructure, Iterable<T> {
-}
