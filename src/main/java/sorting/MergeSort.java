f1f595012ac8d3f227bee44bb86dab66a81b8c9c
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-03-06 17:07:12 +0300

    update Merge sort with few optimizations

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -3,53 +3,44 @@ package sorting;
 import java.util.Arrays;
 
 /**
- * Merge sort implementation
- *
- * @param <T> data type
+ * The top-down implementation of Merge sort
  */
 public class MergeSort<T extends Comparable> implements ArraySort<T> {
 
     @Override
     public void sort(T[] array) {
-        mergeSort(array, 0, array.length - 1);
+        T[] buffer = Arrays.copyOf(array, array.length);
+        mergeSort(array, buffer, 0, array.length - 1);
     }
 
-    private void mergeSort(T[] array, int lo, int hi) {
+    private void mergeSort(T[] array, T[] buffer, int lo, int hi) {
         if (hi <= lo) {
             return;
         }
 
-        int pivot = (lo + hi) / 2;
+        int pivot = lo + (hi - lo) / 2;
 
-        mergeSort(array, lo, pivot);
-        mergeSort(array, pivot + 1, hi);
+        mergeSort(array, buffer, lo, pivot);
+        mergeSort(array, buffer, pivot + 1, hi);
 
-        merge(array, lo, pivot, hi);
+        merge(array, buffer, lo, pivot, hi);
     }
 
-    private void merge(T[] array, int lo, int pivot, int hi) {
-        T[] leftPart = Arrays.copyOfRange(array, lo, pivot + 1);
-        T[] rightPart = Arrays.copyOfRange(array, pivot + 1, hi + 1);
-
-        int leftCursor = 0, rightCursor = 0;
-
+    private void merge(T[] array, T[] buffer, int lo, int pivot, int hi) {
         for (int i = lo; i <= hi; i++) {
-            T min;
+            buffer[i] = array[i];
+        }
 
-            T left = leftCursor < leftPart.length ? leftPart[leftCursor] : null;
-            T right = rightCursor < rightPart.length ? rightPart[rightCursor] : null;
-
-            if (right == null) {
-                min = leftPart[leftCursor++];
-            } else if (left == null) {
-                min = rightPart[rightCursor++];
-            } else if (lessThan(left, right)) {
-                min = leftPart[leftCursor++];
+        for (int i = lo, j = pivot + 1, k = lo; i <= pivot || j <= hi; k++) {
+            if (i > pivot) {
+                array[k] = buffer[j++];
+            } else if (j > hi) {
+                array[k] = buffer[i++];
+            } else if (lessThan(buffer[i], buffer[j])) {
+                array[k] = buffer[i++];
             } else {
-                min = rightPart[rightCursor++];
+                array[k] = buffer[j++];
             }
-
-            array[i] = min;
         }
     }
 
