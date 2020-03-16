79a170bb55cb9a029a3df1701777612293cb9f69
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-03-16 23:38:06 +0300

    add agenda for quick sort implementations

---
+++ src/main/java/sorting/quicksort/QuickSort.java
@@ -0,0 +1,12 @@
+package sorting.quicksort;
+
+/**
+ * to implement
+ * - Lomuto partition scheme
+ * - Hoare parititon scheme
+ * - 2 way quick sort with random pivot
+ * - 2 way quick sort with median 3 pivot
+ * - 3 way quick sort with random pivot
+ */
+public class QuickSort {
+}
